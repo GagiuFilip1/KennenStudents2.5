36c72efaa9b04c331a3f0fa06646dc10e6f14aa4
GagiuFilip1 <gagiu.filip@gmail.com>
2015-11-20 18:09:14 +0200

    KennenStudents2.5
    Stream

---
+++ KennenStudentv1.8-master/StudentsKennen/StudentsKennen/Themes.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace StudentsKennen
+{
+    public partial class Themes : Form
+    {
+        public Themes()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+   
+            
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Themes_Load(object sender, EventArgs e)
+        {
+            timer1.Interval = 8000;
+            timer1.Enabled = true;
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            this.Hide();
+            Form D = new KennenS();
+            D.Show();
+        }
+    }
+}
